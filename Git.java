4b1936a5f401fcff26405d523d3d3738e58cc2fb
Árni Hlynur <arnihl@gmail.com>
2020-04-02 14:45:31 +0000

    login features

--- Git.java
+++ Git.java
@@ -1,24 +1,53 @@
 import java.lang.ProcessBuilder;
+import java.io.BufferedReader;
+import java.lang.Object;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.StringJoiner;
 public class Git {
     public static void main(String [] args){
         String message = args[0];
-        try {
-            ProcessBuilder pb =
-   new ProcessBuilder("myCommand", "myArg1", "myArg2");
- Map<String, String> env = pb.environment();
- env.put("VAR1", "myValue");
- env.remove("OTHERVAR");
- env.put("VAR2", env.get("VAR1") + "suffix");
- pb.directory(new File("myDir"));
- File log = new File("log");
- pb.redirectErrorStream(true);
- pb.redirectOutput(Redirect.appendTo(log));
- Process p = pb.start();
- assert pb.redirectInput() == Redirect.PIPE;
- assert pb.redirectOutput().file() == log;
- assert p.getInputStream().read() == -1;
-        } catch (Exception e) {
-            System.err.println(e);
-        }
+        List<String> gitadd = new ArrayList<>();
+        gitadd.add("git");
+        gitadd.add("add");
+        gitadd.add(".");
+        List<String> commit = new ArrayList<>();
+        commit.add("git");
+        commit.add("commit");
+        commit.add("-m");
+        commit.add(message);
+        List<String> push = new ArrayList<>();
+        push.add("git");
+        push.add("push");
+        push.add("origin");
+        push.add("master");
+        runCommandForOutput(gitadd);
+        runCommandForOutput(commit);
+        runCommandForOutput(push);
+
+        
     }
+
+    public static String runCommandForOutput(List<String> params) {
+        ProcessBuilder pb = new ProcessBuilder(params);
+        Process p;
+        String result = "";
+        try {
+            p = pb.start();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+    
+            StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+            reader.lines().iterator().forEachRemaining(sj::add);
+            result = sj.toString();
+    
+            p.waitFor();
+            p.destroy();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
 }
