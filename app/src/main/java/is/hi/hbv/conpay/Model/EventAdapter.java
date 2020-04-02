859f6afc8bbc65897940fb49323d4b0b4146b61c
Karl Sigurvinsson <kalli94@gmail.com>
2020-04-02 18:46:53 +0000

    vinna í 'learn more' takkanum

--- app/src/main/java/is/hi/hbv/conpay/Model/EventAdapter.java
+++ app/src/main/java/is/hi/hbv/conpay/Model/EventAdapter.java
@@ -1,10 +1,13 @@
 package is.hi.hbv.conpay.Model;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +21,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
 
     private Context mCtx;
     private List<Event> eventList;
+    Dialog viewSingleEvent;
 
 
     public EventAdapter(Context mCtx, List<Event> eventList) {
@@ -32,6 +36,11 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
         LayoutInflater inflater = LayoutInflater.from(mCtx);
         View view = inflater.inflate(R.layout.layout_events, null);
         EventViewHolder holder = new EventViewHolder(view);
+
+        // búa til nýjan hlut fyrir "learn more" takkann
+        viewSingleEvent = new Dialog(mCtx);
+        viewSingleEvent.setContentView(R.layout.layout_single_event);
+
         return holder;
 
     }
@@ -42,6 +51,25 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
         holder.textViewTitle.setText(event.getName());
         holder.textViewDesc.setText(event.getDescription());
         holder.textViewPrice.setText(String.valueOf(event.getPriceCat()));
+        // onclick handler þegar ýtt er á VIEW takkan í cardviewinu:
+        holder.btnViewDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textViewTitle = viewSingleEvent.findViewById(R.id.textViewTitle);
+                TextView textViewDesc = viewSingleEvent.findViewById(R.id.textViewShortDesc);
+                TextView textViewPrice = viewSingleEvent.findViewById(R.id.textViewPrice);
+                TextView textViewMaxPart = viewSingleEvent.findViewById(R.id.textViewMaxPart);
+                TextView textViewMinPart = viewSingleEvent.findViewById(R.id.textViewMinPart);
+                TextView textViewExpDate = viewSingleEvent.findViewById(R.id.textViewExpDate);
+                textViewExpDate.setText(String.valueOf(event.geteDate()));
+                textViewTitle.setText(event.getName());
+                textViewDesc.setText(event.getDescription());
+                textViewPrice.setText(String.valueOf(event.getPriceCat()));
+                textViewMaxPart.setText(String.valueOf(event.getMaxParticipants()));
+                textViewMinPart.setText(String.valueOf(event.getMinParticipants()));
+                viewSingleEvent.show();
+            }
+        });
     }
 
     @Override
@@ -51,11 +79,13 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
 
     public class EventViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textViewTitle, textViewDesc, textViewRating, textViewPrice;
+        Button btnViewDetails;
+        TextView textViewTitle, textViewDesc, textViewPrice;
 
         public EventViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            btnViewDetails = itemView.findViewById(R.id.buttonViewDetailsEvent);
             textViewTitle = itemView.findViewById(R.id.textViewTitle);
             textViewDesc = itemView.findViewById(R.id.textViewShortDesc);
             textViewPrice = itemView.findViewById(R.id.textViewPrice);
